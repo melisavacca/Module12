52f9beb9e2dc8f96d0ed0f50b1d300288e7977c6
Melisa Vacca <melisa.vacca@gmail.com>
2021-09-13 13:28:49 -0400

    updating

--- plots.js
+++ plots.js
@@ -1,38 +1,38 @@
-var trace = {
-    x: ["burrito", "pizza", "chicken"],
-    y: [10, 18, 5],
-    type: "bar"
-};
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
 
-var layout = {
-    title: "Luncheon Survey",
-    xaxis: {title: "Food Option"},
-    yaxis: {title: "Number of Respondents"}
-};
+  function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+  }
 
-Plotly.newPlot("plotArea", [trace], layout);
-
-var trace = {
-    x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-    y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-    type: "bar"
-};
-   var data = [trace];
-   var layout = {
-    title: "'Bar' Chart",
-    xaxis: { title: "Drinks"},
-    yaxis: { title: "% of Drinks Ordered"}
-};
-Plotly.newPlot("plotArea", data, layout);
-
-var trace = {
-    labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
-    "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-    values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-    type: 'pie'
-};
-   var data = [trace];
-   var layout = {
-    title: "'Pie' Chart",
-};
-Plotly.newPlot("plotArea", data, layout);
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      PANEL.append("h2").text(result.id);
+      PANEL.append("h3").text(result.ethnicity);
+      PANEL.append("h4").text(result.gender);
+      PANEL.append("h5").text(result.age);
+      PANEL.append("h6").text(result.location);
+      PANEL.append("h7").text(result.bbtype);
+      PANEL.append("h8").text(result.wfreq);
+    });
+  }
